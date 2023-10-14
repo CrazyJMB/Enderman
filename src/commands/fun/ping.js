875ef0281ff6f270f14112d23937d105b14ce20a
CrazyJMB <crazyjmbemail@gmail.com>
2023-10-14 02:26:30 +0200

    Registro de commandos en el servidor

--- src/commands/fun/ping.js
+++ src/commands/fun/ping.js
@@ -3,7 +3,10 @@ const { SlashCommandBuilder } = require("discord.js");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("ping")
-    .setDescription("Replies with Pong!"),
+    .setDescription("Replies with Pong!")
+    .setDescriptionLocalizations({
+      SpanishES: "Responde con un Pong!",
+    }),
   async execute(interaction) {
     await interaction.reply("Pong!");
   },
--- src/commands/utility/server.js
+++ src/commands/utility/server.js
@@ -1,13 +1,35 @@
 const { SlashCommandBuilder } = require("discord.js");
+const axios = require("axios");
+require("dotenv").config();
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("server")
-    .setDescription("Provides information about the server."),
+    .setDescription("Provides information about the server.")
+    .setDescriptionLocalizations({
+      SpanishES: "Información sobre el servidor",
+    }),
   async execute(interaction) {
-    // interaction.guild is the object representing the Guild in which the command was run
-    await interaction.reply(
-      `This server is ${interaction.guild.name} and has ${interaction.guild.memberCount} members.`
-    );
+    const address = process.env.IP;
+
+    try {
+      const response = await axios.get(`https://api.mcsrvstat.us/3/${address}`);
+      const serverData = response.data;
+
+      if (serverData.online) {
+        await interaction.reply(
+          `El servidor ${serverData.motd.clean.join(
+            " "
+          )} está en línea y tiene ${serverData.players.online} jugadores.`
+        );
+      } else {
+        await interaction.reply(`El servidor está actualmente desconectado.`);
+      }
+    } catch (error) {
+      console.error(error);
+      await interaction.reply(
+        "Hubo un error al obtener la información del servidor."
+      );
+    }
   },
 };
